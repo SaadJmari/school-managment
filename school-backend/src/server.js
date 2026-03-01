cfa4383870d108349732b099332b9b7deda729d9
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2026-03-01 13:22:40 +0000

    Move MongoDB URI to env vars

--- school-backend/src/server.js
+++ school-backend/src/server.js
@@ -1,14 +1,20 @@
 // Import the Express library
 const mongoose = require('mongoose');
-const app = require("./app.js")
+const app = require("./app.js");
+require("dotenv").config();
 
 // Define port for the server to listen on
 const PORT = 5000;
 
 // Connect to MongoDB
-mongoose.connect(
-    REMOVED_MONGODB_URI
-)
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if(!MONGODB_URI) {
+    throw new Error("Missing MONGODB_URI in environment variables");
+}
+
+
+mongoose.connect(MONGODB_URI)
 .then(()=> {
     console.log('Connected to MongoDB Atlas');
     // Make the server listen on the port
